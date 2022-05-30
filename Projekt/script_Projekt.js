4c0d107f095f5644a38ff5d6c90c04e12fe266d1
DESKTOP-FB3SAQO\renne <sullivan.renner@edu.gbssg.ch>
2022-05-30 22:24:14 +0200

    Projekt - Überarbeitung Validierung Email-Adresse

--- Projekt/script_Projekt.js
+++ Projekt/script_Projekt.js
@@ -50,6 +50,9 @@ function checkemail()
         return false;
     }
     let email = document.getElementById('email').value
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
+    return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 }
